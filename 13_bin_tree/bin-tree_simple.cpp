bdb6b12680ff910a6d74a6d0643c448809cf1ad6
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-04-22 16:41:37 +0700

    add(tree)

--- 13_bin_tree/bin-tree_simple.cpp
+++ 13_bin_tree/bin-tree_simple.cpp
@@ -1,238 +1,220 @@
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-using namespace std;
+#include <list>
+// using namespace std;
 
-struct Elem
-{
-    int key;
-    Elem* left, *right, *parent;
+struct Pair {
+	int key;
+	char value;
 };
 
-class Tree
-{
-    //корень
-    Elem* root_ = nullptr;
+class ElemTree {
+	struct Node {
+		Pair* element;
+		Node* left = nullptr;
+		Node* right = nullptr;
+		Node* parent = nullptr;
+	};
+
+	Node* root_ = nullptr;
 
 public:
-    ~Tree();
-    //Печасть всего содержимого
-    void Print();
-    //печать от указанного узла
-    void Print(Elem*);
-    //поиск от указанного узла
-    Elem* Search(Elem*, int key);
-    //min от указанного узла
-    Elem* Min(Elem*);
-    //max от указанного узла
-    Elem* Max(Elem*);
-    //следующий для указанного узла
-    Elem* Next(Elem*);
-    //предыдущий для указанного узла
-    Elem* Previous(Elem*);
-    //вставка узла
-    void Insert(Elem*);
-    //удаление ветки для указанного узла,
-    //0 - удаление всего дерева
-    void Delete(Elem*);
-    //получить корень
-    Elem* GetRoot();
+	bool IsEmpty() {
+		return root_ == nullptr; 
+	}
+	void Insert(Pair* elem) {
+		Node* obj = new Node{ elem };
+
+		if (IsEmpty()) {
+			root_ = obj;
+			return;
+		}
+
+		Node* temp = root_;
+		Node* curParent = nullptr;
+		bool flag = true;
+		do {
+			curParent = temp;
+			flag = obj->element->key < temp->element->key;
+			if (flag)
+				temp = temp->left;
+			else
+				temp = temp->right;
+		} while (temp);
+
+		obj->parent = curParent;
+		if (flag)
+			curParent->left = obj;
+		else
+			curParent->right = obj;
+	}
+	/// TODO: fix last ','
+	void operator>> (std::ostream& out) {
+		Print(root_, out);
+		out << "\n";
+	}
+	Pair* Search(int key) {
+		return Search(key, root_)->element;
+	}
+	std::list<Pair*> SearchAll(int key) {
+		std::list<Pair*> list;
+		Node* temp = Search(key, root_);
+		while (temp->element->key == key) {
+			list.push_back(temp->element);
+			temp = Next(temp);
+		}
+		return list;
+	}
+	void Remove(int key) {
+		Delete(Search(key, root_));
+		// Remove(Search(key, root_));
+	}
+private:
+	void Print(Node* obj, std::ostream& out) {
+		if (obj == nullptr)
+			return;
+
+		Print(obj->left, out);
+		out << obj->element->value << "["
+		    << obj->element->key << "], ";
+		Print(obj->right, out);
+	}
+	Node* Min(Node* obj) {
+		while (obj->left)
+			obj = obj->left;
+		return obj;
+	}
+	Node* Next(Node* obj) {
+		if (!obj) // obj == nullptr
+			throw "can't find next from null";
+
+		if (obj->right){
+			return Min(obj->right);
+		}
+		Node* curParent = obj->parent;
+		while (obj == curParent->right && curParent != nullptr) {
+			obj = curParent;
+			curParent = curParent->parent;
+		}
+		return curParent;
+	}
+	Node* Search(int key, Node* temp){
+		while(temp){
+			int tempKey = temp->element->key;
+			if (key == tempKey)
+				break;
+			if (key < tempKey)
+				temp = temp->left;
+			else
+				temp = temp->right;
+		}
+		return temp;
+	}
+	void Remove(Node* obj) {
+		if (!obj)
+			throw "nothing to delete\n";
+		
+		if (root_ == obj) { // delete root
+			if (obj->left == 0 && obj->right == 0)
+				root_ = nullptr;
+			else if (obj->left != 0 && obj->right != 0) {
+				root_ = obj->right;
+				obj->right->parent = nullptr;
+
+				Node* temp = Next(obj);
+				temp->left = obj->left;
+				obj->left->parent = temp;
+			}
+			else if (obj->left != 0) {
+				obj->left->parent = nullptr;
+				root_ = obj->left;
+			}
+			else {
+				obj->right->parent = nullptr;
+				root_ = obj->right;
+			}
+		}
+		else if (obj->left == 0 && obj->right == 0) {
+			if (obj == obj->parent->right)
+				obj->parent->right = nullptr;
+			else
+				obj->parent->left = nullptr;
+		}
+		else if (obj->left != 0 && obj->right != 0) {
+			if (obj == obj->parent->right)
+				obj->parent->right = obj->right;
+			else
+				obj->parent->left = obj->right;
+			obj->right->parent = obj->parent;
+
+			Node* temp = Next(obj);
+			temp->left = obj->left;
+			obj->left->parent = temp;
+		}
+		else if (obj->left != 0) {
+			obj->left->parent = obj->parent;
+			obj->parent->left = obj->left;
+		}
+		else {
+			obj->right->parent = obj->parent;
+			obj->parent->right = obj->right;
+		}
+		delete obj;
+	}
+	void Delete(Node* obj){
+		if (!obj)
+			throw "nothing to delete\n";
+		
+		Node* temp, *curParent;
+		if (obj->left == nullptr || obj->right == nullptr)
+			curParent = obj;
+		else
+			curParent = Next(obj);
+
+		if (curParent->left)
+			temp = curParent->left;
+		else
+			temp = curParent->right;
+
+		if (temp)
+			temp->parent = curParent->parent;
+
+		if ( !(curParent->parent) )
+			root_ = temp;
+		else if (curParent == curParent->parent->left)
+			curParent->parent->left = temp;
+		else
+			curParent->parent->right = temp;
+
+		if (curParent != obj) {
+			obj->element = curParent->element;
+		}
+		delete curParent;
+	}
 };
-//Рекурсивный обход дерева
-void Tree::Print(Elem* node) {
-    if (node == 0)
-        return;
-
-    Print(node->left);
-    cout << node->key << " ";
-    Print(node->right);
-}
-void Tree::Print() {
-    cout << "Tree values: ";
-    Print(root_);
-    cout << endl;
-}
-Elem* Tree::Search(Elem* node, int key) {
-    //Пока есть узлы и ключи не совпадают
-    while (node != 0 && key != node->key)
-    {
-        if (key < node->key)
-            node = node->left;
-        else
-            node = node->right;
-    }
-    return node;
-}
-Elem* Tree::Min(Elem* node) {
-    //Поиск самого "левого" узла
-    if (node != 0)
-        while (node->left != 0)
-            node = node->left;
-    return node;
-}
-Elem* Tree::Max(Elem* node) {
-    //Поиск самого "правого" узла
-    if (node != 0)
-        while (node->right != 0)
-            node = node->right;
-    return node;
-}
-Elem* Tree::Next(Elem* node) {
-    if (node == 0)
-        return nullptr;
-
-    //если есть правый потомок
-    if (node->right != 0)
-        return Min(node->right);
-
-    //родитель узла
-    Elem* parent = node->parent;
-    //если node не корень и node справа
-    while (parent != 0 && node == parent->right)
-    {
-        //Движемся вверх
-        node = parent;
-        parent = node->parent;
-    }
-    return parent;
-}
-Elem* Tree::Previous(Elem* node) {
-    if (node == 0)
-        return nullptr;
-
-    //если есть левый потомок
-    if (node->left != 0)
-        return Max(node->left);
-
-    //родитель узла
-    Elem* parent = node->parent;
-    //если node не корень и node слева
-    while (parent != 0 && node == parent->left)
-    {
-        //Движемся вверх
-        node = parent;
-        parent = node->parent;
-    }
-    return parent;
-}
-Elem* Tree::GetRoot() {
-    return root_;
-}
-void Tree::Insert(Elem* node) {
-    //потомков нет
-    node->left = nullptr;
-    node->right = nullptr;
-
-    //поиск места
-    if (root_ == 0) {//элемент первый (единственный)
-        root_ = node;
-        return;
-    }
-        
-    Elem* temp = root_;
-    Elem* parent = nullptr;
-    bool flag;
-    while (temp != 0)
-    {
-        //будущий родитель
-        parent = temp;
-        flag = node->key < temp->key;
-        if (flag)
-            temp = temp->left;
-        else
-            temp = temp->right;
-    }
-    //заполняем родителя
-    node->parent = parent;
-    //чей ключ больше?
-    if (flag)
-        parent->left = node;
-    else
-        parent->right = node;
-}
-void Tree::Delete(Elem* node) {
-    Elem* temp, *toDelete;
-    //не 2 потомка
-    if (node->left == 0 || node->right == 0)
-        toDelete = node;
-    else
-        toDelete = Next(node);
-
-    if (toDelete->left != 0)
-        temp = toDelete->left;
-    else
-        temp = toDelete->right;
-
-    if (temp != 0)
-        temp->parent = toDelete->parent;
-
-    // Запись в родителя
-    if (toDelete->parent == 0)
-        //Удаляется корневой узел?
-        root_ = temp;
-    else if (toDelete == toDelete->parent->left)
-        //слева от родителя?
-        toDelete->parent->left = temp;
-    else
-        //справа от родителя?
-        toDelete->parent->right = temp;
-
-    if (toDelete != node)
-    {
-        //Копирование данных узла
-        node->key = toDelete->key;
-    }
-    delete toDelete;
-}
-Tree::~Tree() {
-    while (root_ != 0)
-        Delete(root_);
-    cout << "~tree_removed\n";
-}
 
 int main()
 {
-    srand(time(0));
-    {
-        int size = 10;
-        Tree arr;
-        for(int i=0; i != size; ++i){
-            arr.Insert(new Elem{rand()%50});
-        }
-        //Вывод результатов
-        arr.Print();
+	using namespace std;
 
-        cout << "less 10 items: ";
-        Elem* mid;
-        Elem* el = arr.Min(arr.GetRoot());
-        for(int i=0; i != size; ++i){
-            int x = el->key;
-            if (x < 10)
-                cout << x << " ";
-            el = arr.Next(el);
-            if (i+1 == size/2)
-                mid = el;
-        }
-        cout << "\n";
+	ElemTree tree;
+	Pair pairs[] = {{5, 'a'}, {1, 'b'}, {8, 'c'}, {1, 'd'}};
 
-        cout << mid->key << " removed\n";
-        arr.Delete(mid);        
-        arr.Print();
-    }
-    {
-        int size = 20;
-        Tree arr;
-        for(int i=0; i != size; ++i) {
-            arr.Insert(new Elem{rand()%10});
-        }
-        //Вывод результатов
-        arr.Print();
+	for (size_t i = 0; i < 4; i++)
+		tree.Insert(&pairs[i]);
+	tree >> cout;
 
-        if (arr.Search(arr.GetRoot(), 0) == nullptr)
-            cout << "has no '0' item\n";
-        else
-            cout << "arr has item '0'\n";
-    }
+	std::list<Pair*> list = tree.SearchAll(1);
+	cout << "found [1]: ";
+	for (Pair* p : list)
+		cout << p->value << " ";
+	cout << endl;
+	
+	tree.Remove(5);
+	tree >> cout;
+
+	tree.Remove(1);
+	tree >> cout;
+
+	tree.Remove(8);
+	tree >> cout;
 }
